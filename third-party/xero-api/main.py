ea427e4fd26ee6471a4014e6f63247578ca6154b
MalshaL <malsha.13@cse.mrt.ac.lk>
2022-09-29 11:57:02 +1000

    Added dockerfiles

--- third-party/xero-api/main.py
+++ third-party/xero-api/main.py
@@ -1,15 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 import json
 
 app = Flask(__name__)
 
 
-@app.route("/financialStatements/balanceSheet", methods=['POST'])
+@app.route("/financialStatements/balanceSheet", methods=['POST', 'OPTIONS'])
 def get_balance_sheet():
-    data_file = open('mock-data.json')
-    data = json.load(data_file)
-    data_file.close()
-    return jsonify({'data': data})
+    if request.method == 'OPTIONS':
+        return _build_preflight_response()
+    elif request.method == 'POST':
+        data_file = open('mock-data.json')
+        data = json.load(data_file)
+        data_file.close()
+        return _build_response(jsonify({'data': data}))
+
+
+# fix cors - response for POST call
+def _build_response(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+# fix cors - response for OPTIONS call
+def _build_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
 
 if __name__ == '__main__':
